cf09ef6b04833a7b7901bc01296e02886b5664dd
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-01-02 16:26:53 +0100

    Creating a Blockchain with Etherium

---
+++ src/components/Services.jsx
@@ -0,0 +1,9 @@
+const Services = () => {
+  return (
+    <div>
+      <h1>Services Component</h1>
+    </div>
+  );
+};
+
+export default Services;
---
+++ src/components/Transactions.jsx
@@ -0,0 +1,9 @@
+const Transactions = () => {
+  return (
+    <div>
+      <h1>Transactions Component</h1>
+    </div>
+  );
+};
+
+export default Transactions;
