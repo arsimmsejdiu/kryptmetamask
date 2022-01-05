d8d3cf27735dc9bbf736b567f3a4f78bb648775f
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-01-05 22:44:29 +0100

    added the latest transacction section

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,7 +1,30 @@
+import React, { useContext } from "react";
+
+import { TransactionContext } from "../context/TransactionContext";
+
+//import useFetch from "../hooks/useFetch.hooks";
+import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
+import TransactionCard from "./TransactionCard";
+
 const Transactions = () => {
+  const { currentAccount } = useContext(TransactionContext);
+
   return (
-    <div>
-      <h1>Transactions Component</h1>
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount ? (
+          <h3 className="text-white text-3xl text-center my-2">Latest Transactions</h3>
+        ) : (
+          <h3 className="text-white text-3xl text-center my-2">Connect your account to see your latest transactions</h3>
+        )}
+        
+        <div className=" flex flex-wrap justify-center items-center mt-10">
+        {dummyData.reverse().map((transaction, i) => (
+          <TransactionCard key={i} { ...transaction}/>
+        ))}
+        </div>
+      </div>
     </div>
   );
 };
