6528a945f3c3e27c95cdeaf78694e0ceb747eb1b
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-01-08 13:42:45 +0100

    finished displaying all transactions that has been made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           <NavBarItem key={item + index} title={item} />
         ))}
         <li className="bg-[#2951e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
+          Connect Wallet
         </li>
       </ul>
       <div className="flex relative ">
